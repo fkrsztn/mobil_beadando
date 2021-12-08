89ce9ab5e847585e178de934038ced44e54ff0d8
mistybaker <farkask4826@gmail.com>
2021-12-08 02:13:57 +0100

    Quiz app

---
+++ Quiz_Beadando/QuizData2.js
@@ -0,0 +1,32 @@
+export default data2 = [
+    {
+        question: "Melyik regényt írta Mikszáth Kálmán?",
+        options: ["A Fekete város","Az aranyember","Utas és holdvilág"],
+        correct_option: "A Fekete város"
+    },
+    {
+        question: "Melyik verset NEM Ady Endre írta?",
+        options: ["Góg és Magóg fia vagyok én","A magyar ugaron","Körúti hajnal"],
+        correct_option: "Körúti hajnal"
+    },
+    {
+        question: "Melyik írónk sírja található az Egri várban?",
+        options: ["Balassi Bálint","Heltai Gáspár","Gárdonyi Géza"],
+        correct_option: "Gárdonyi Géza"
+    },
+    {
+        question: "Ki született a költők közül a legkorábban?",
+        options: ["Tóth Árpád","Berzsenyi Dániel","Kányádi Sándor"],
+        correct_option: "Berzsenyi Dániel"
+    },
+    {
+        question: "Kinek a múzsája volt Lilla?",
+        options: ["Ady Endre","Csokonai Vitéz Mihály","József Attila"],
+        correct_option: "Csokonai Vitéz Mihály"
+    },
+    {
+        question: "Az alábbiak közül ki a legnépszerűbb gyermekvers író?",
+        options: ["Weöres Sándor","Karinthy Frigyes","Kassák Lajos"],
+        correct_option: "Weöres Sándor"
+    }
+]
---
+++ Quiz_Beadando/QuizData3.js
@@ -0,0 +1,38 @@
+export default data3 = [
+    {
+        question: "Melyik sportágban vannak 20 perces harmadok?",
+        options: ["Kosárlabda","Jégkorong","Kézilabda"],
+        correct_option: "Jégkorong"
+    },
+    {
+        question: "Melyik sportágban vannak egy csapatból hatan a pályán?",
+        options: ["Kézilabda","Röplabda","Vízilabda"],
+        correct_option: "Röplabda"
+    },
+    {
+        question: "Melyik sportot nem labdával játszák?",
+        options: ["Lacrosse","Hurling","Curling"],
+        correct_option: "Curling"
+    },
+    {
+        question: "Melyik sportágban tart egy meccs 32 perc tiszta játékidőig?",
+        options: ["Jégkorong","Kosárlabda","Vízilabda"],
+        correct_option: "Vízilabda"
+    },
+    {
+        question: "Melyik magyar focista játszott 2010-ben Európa Liga-döntőt?",
+        options: ["Dárdai Pál","Gera Zoltán","Juhász Roland"],
+        correct_option: "Gera Zoltán"
+    },
+    {
+        question: "Kinek a góljánál hangzott el ez a mondat az EB-n: „Be lehet azt piszkálni, pöckölni, tuszkolni”?",
+        options: ["Szalai Ádám","Gera Zoltán","Stieber Zoltán"],
+        correct_option: "Szalai Ádám"
+    },
+    {
+        question: "Ki szerezte azt a gólt, amelyet Puskás-díjjal jutalmazott 2019-ben a FIFA?",
+        options: ["Zsóri Dániel","Gera Zoltán","Sallai Roland"],
+        correct_option: "Zsóri Dániel"
+    }
+    
+]
